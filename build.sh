f0835caf1cd6ec1015ae77ae76b64d84500d224e
Favour Samuel <your_email@example.com>
2026-03-15 16:33:23 +0100

    Switch to CDN and simplify build to fix loading hang

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
-# exit on error
 set -o errexit
 
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Auto-create admin
 python manage.py shell -c "from django.contrib.auth.models import User; User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@example.com', 'HavyBoutique2026!')"
