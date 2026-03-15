a6e2cbfe679d3c9d4de1760c33ebc993b02cad77
Favour Samuel <your_email@example.com>
2026-03-15 16:30:13 +0100

    Switch to CDN and simplify build to fix loading hang

--- Havy_Ecommercesite/settings.py
+++ Havy_Ecommercesite/settings.py
@@ -119,8 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR.parent/ "static",]
-
+STATICFILES_DIRS = [BASE_DIR / "static"]
 STORAGES = {
     "staticfiles": {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
